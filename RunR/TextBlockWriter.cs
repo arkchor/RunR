4d1091dcc133a4d8131aad7c140954b3d239df53
arkadiusz.chorian <arkadiusz.chorian@gmail.com>
2020-03-23 08:33:38 +0100

    Dispatcher added when writing to console textbox.

--- RunR/TextBlockWriter.cs
+++ RunR/TextBlockWriter.cs
@@ -1,11 +1,15 @@
 ﻿using System.IO;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace Archersoft.RunR
 {
     public class TextBlockWriter : TextWriter
     {
+        private static readonly Dispatcher Dispatcher = Application.Current.Dispatcher;
+
         private readonly TextBox _textBox;
 
         public TextBlockWriter(TextBox textBox)
@@ -15,16 +19,20 @@ namespace Archersoft.RunR
 
         public override void Write(char value)
         {
-            _textBox.Text += value;
-            _textBox.ScrollToEnd();
-
+            Dispatcher.Invoke(() =>
+            {
+                _textBox.Text += value;
+                _textBox.ScrollToEnd();
+            });
         }
 
-        public override void Write(string? value)
+        public override void Write(string value)
         {
-            _textBox.Text += value;
-            _textBox.ScrollToEnd();
-
+            Dispatcher.Invoke(() =>
+            {
+                _textBox.Text += value;
+                _textBox.ScrollToEnd();
+            });
         }
 
         public override Encoding Encoding
